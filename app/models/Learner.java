17a0ca18c9d891f2373626f947eed6411d45e5e9
Max Virgil <maxtvirgil@gmail.com>
2015-02-18 23:51:26 -0500

    basic learner creation.  goto /testlearners after logging in

--- app/models/Learner.java
+++ app/models/Learner.java
@@ -1,11 +1,14 @@
 package models;
 
+import java.util.ArrayList;
 import java.util.List;
 
-import play.db.ebean.*;
-import play.data.validation.Constraints.*;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
-import javax.persistence.*;
+import play.data.validation.Constraints.Required;
+import play.db.ebean.Model;
 
 @Entity
 @SuppressWarnings("serial")
@@ -15,7 +18,10 @@ public class Learner extends Model {
     public Long id;
 
     @Required
-    public String fullname;
+    public String firstName;
+    
+    @Required
+    public String lastName;
     
     @Required
     public List<Session> sessions;
@@ -26,5 +32,25 @@ public class Learner extends Model {
 
     public static Finder<Long, Learner> find = new Finder<Long, Learner>(
             Long.class, Learner.class);
-
+    
+    public Learner(String first, String last, User owner) {
+        this.firstName = first;
+        this.lastName = last;
+        this.owner = owner;
+        this.sessions = new ArrayList<>();
+    }
+    
+    public static void create(String firstName, String lastName, String ownerEmail) {
+        User owner = User.find.ref(ownerEmail);
+        Learner newLearner = new Learner(firstName, lastName, owner);
+        newLearner.save();
+    }
+    
+    public static List<Learner> getAll() {
+        return Learner.find.all();
+    }
+    
+    public static List<Learner> getAllOwnedBy(String ownerEmail) {
+        return Learner.find.where().eq("owner.email", ownerEmail).findList();
+    }
 }
